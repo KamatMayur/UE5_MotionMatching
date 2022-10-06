03f5dd96e6edd1dfea757f18005ffe55ce5d48ff
KamatMayur <75731293+KamatMayur@users.noreply.github.com>
2022-10-06 14:22:48 +0530

    Added Motion Schema
    This class keeps the important settings for motion matching

--- Source/MotionMatching/MotionMatching.cpp
+++ Source/MotionMatching/MotionMatching.cpp
@@ -4,4 +4,21 @@
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MotionMatching, "MotionMatching" );
- 
+
+TArray<FName> UMotionSchema::GetBoneOptions() const
+{
+	
+
+	TArray<FName> AllBones;
+	if (Skeleton)
+	{
+
+		for (int32 i = 0; i < Skeleton->GetReferenceSkeleton().GetNum(); ++i)
+			AllBones.Insert(Skeleton->GetReferenceSkeleton().GetBoneName(i), i);
+		return AllBones;
+	}
+
+	else return TArray<FName>();
+	
+    
+}
--- Source/MotionMatching/MotionMatching.h
+++ Source/MotionMatching/MotionMatching.h
@@ -1,5 +1,45 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #pragma once
-
+#include "Engine/DataAsset.h"
 #include "CoreMinimal.h"
+#include "MotionMatching.generated.h"
+
+
+UCLASS()
+class MOTIONMATCHING_API UMotionSchema : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	// The skeleton whose animations and bones will be used in motion matching 
+	UPROPERTY(EditAnywhere, Category = "Motion Schema")
+		USkeleton* Skeleton;
+	
+	// The bones to be used in motion matching
+	UPROPERTY(EditAnywhere, Category = "Motion Schema", meta = (GetOptions = "GetBoneOptions"))
+		TArray<FName> Bones;
+
+	// Just creates a drop-down of all bones that the Skeleton has
+	UFUNCTION(CallInEditor)
+		TArray<FName> GetBoneOptions() const;
+
+	UPROPERTY(EditAnywhere, Category = "Motion Schema", meta = (ClampMin = "1", ClampMax = "60"))
+		float SampleRate = 10.f;
+
+	UPROPERTY(EditAnywhere, Category = "Motion Schema")
+		bool bUseBoneVelocities;
+
+	UPROPERTY(EditAnywhere, Category = "Motion Schema")
+		bool bUseTrajectoryVelocities;
+
+	UPROPERTY(EditAnywhere, Category = "Motion Schema")
+		bool bUseTrajectoryPositions;
+
+	UPROPERTY(EditAnywhere, Category = "Motion Schema")
+		TArray<float> PoseSampleTimes;
+
+	UPROPERTY(EditAnywhere, Category = "Motion Schema")
+		TArray<float> TrajectorySampleTimes;
+
+};
